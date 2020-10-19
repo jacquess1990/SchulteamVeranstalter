7efc861d30fc7b3a0dd2d120005bb78cc397c3db
jacquess1990 <69588190+jacquess1990@users.noreply.github.com>
2020-10-19 19:27:27 +0200

    Something new

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("main.fxml"));
+        // should be using different approach
         Parent root = loader.load();
             primaryStage.setTitle("Schulteam Veranstalter");
             primaryStage.setScene(new Scene(root, 800, 600));
